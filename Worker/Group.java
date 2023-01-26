c4f326706b14e769b58ca5c80f5e2d4800b46628
Amirova Milyausha <mila17.86@mail.ru>
2023-01-26 13:51:42 +0300

    Add menu

---
+++ Worker/Group.java
@@ -0,0 +1,14 @@
+package Worker;
+
+public class Group extends Department {
+    
+    public Group(String GropName){
+        setDeptName(GropName);
+    }
+
+    public void addPeople(Persona persona){
+        personaInDept.add(persona);
+    }
+  
+
+}
